af83fc767e31fdd7f51221fcb723d34c3c332c8b
Spec <82361338+spectacle-cat@users.noreply.github.com>
2025-05-06 08:29:05 +0100

    starting tiled experiment

---
+++ sprites/tiled/grassland.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="grassland" tilewidth="1000" tileheight="1000" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../boulder.png" width="200" height="200"/>
+ </tile>
+ <tile id="1">
+  <image source="../exit.png" width="200" height="200"/>
+ </tile>
+ <tile id="2">
+  <image source="../tile_floor.png" width="200" height="200"/>
+ </tile>
+ <tile id="3">
+  <image source="../tuft_1.png" width="280" height="240"/>
+ </tile>
+ <tile id="4">
+  <image source="../tuft_4.png" width="280" height="240"/>
+ </tile>
+ <tile id="5">
+  <image source="../tuft_2.png" width="280" height="240"/>
+ </tile>
+ <tile id="6">
+  <image source="../tuft_3.png" width="280" height="240"/>
+ </tile>
+ <tile id="7">
+  <image source="../nest_empty.png" width="200" height="200"/>
+ </tile>
+ <tile id="8">
+  <image source="../nest.png" width="1000" height="1000"/>
+ </tile>
+ <tile id="9">
+  <image source="../EGG_count_3.png" width="149" height="110"/>
+ </tile>
+ <tile id="10">
+  <image source="../EGG_count_shade_1.png" width="149" height="110"/>
+ </tile>
+ <tile id="11">
+  <image source="../EGG_count_shade_2.png" width="149" height="110"/>
+ </tile>
+ <tile id="12">
+  <image source="../EGG_count_shade_3.png" width="149" height="110"/>
+ </tile>
+ <tile id="13">
+  <image source="../EGG_1.png" width="200" height="200"/>
+ </tile>
+ <tile id="14">
+  <image source="../EGG_2.png" width="200" height="200"/>
+ </tile>
+ <tile id="15">
+  <image source="../EGG_3.png" width="200" height="200"/>
+ </tile>
+ <tile id="16">
+  <image source="../EGG_count_1.png" width="149" height="110"/>
+ </tile>
+ <tile id="17">
+  <image source="../EGG_count_2.png" width="149" height="110"/>
+ </tile>
+</tileset>
